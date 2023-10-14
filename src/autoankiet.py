991c401c623bb1c03b7fc8c384661d77652d4acb
mimovnik <jkwidzinski@gmail.com>
2023-10-14 18:14:52 +0200

    Submit a form

--- src/autoankiet.py
+++ src/autoankiet.py
@@ -16,80 +16,74 @@ classes_understandable_whatever_td_xpath = '//*[@id="i1:form_pollAnswer:question
 classes_intresting_whatever_td_xpath = '//*[@id="i1:form_pollAnswer:questions_panel:6:pnl_anwsersLong"]/table/tbody/tr[6]/td'
 contact_availability_whatever_td_xpath = '//*[@id="i1:form_pollAnswer:questions_panel:7:pnl_anwsersLong"]/table/tbody/tr[6]/td'
 social_behavior_whatever_td_xpath = '//*[@id="i1:form_pollAnswer:questions_panel:8:pnl_anwsersLong"]/table/tbody/tr[6]/td'
+submit_form_xpath='//*[@id="i1:form_pollAnswer"]'
+submit_input_class_name='simpleButton'
+submit_value = 'Wyślij odpowiedź'
+confirm_span_xpath='//*[@id="i1:modal_confirmSendContent"]'
+confirm_input_class_name='simpleButton'
+confirm_value= 'Wyślij odpowiedź'
 
-wait_to_load_time = 2
+wait_timeout=3600
 
-def enter_forms(driver):
-    fill_forms_link = driver.find_element(By.XPATH, fill_forms_link_xpath)
-    fill_forms_link.click()
+def click(driver, xpath):
+    WebDriverWait(driver, wait_timeout).until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    clickable = driver.find_element(By.XPATH, xpath)
+    clickable.click();
 
 def fill_forms(driver):
+    WebDriverWait(driver, wait_timeout).until(EC.visibility_of_element_located((By.XPATH, forms_table_xpath)))
     forms_table = driver.find_element(By.XPATH, forms_table_xpath)
     forms = forms_table.find_elements(By.TAG_NAME, 'tr')
     for form in forms:
+        WebDriverWait(driver, wait_timeout).until(EC.visibility_of_element_located((By.XPATH, forms_table_xpath)))
         form.click()
-        time.sleep(wait_to_load_time)
         fill_form(driver)
         input()
 
 def fill_form(driver):
-    wait_to_fill_time = 0.1
+    click_radio_button(driver, attendance_major_td_xpath)
+    click_radio_button(driver, location_at_university_td_xpath)
+    click_radio_button(driver, according_to_rules_whatever_td_xpath)
+    click_radio_button(driver, according_to_course_card_whatever_td_xpath)
+    click_radio_button(driver, results_on_time_whatever_td_xpath)
+    click_radio_button(driver, classes_understandable_whatever_td_xpath)
+    click_radio_button(driver, classes_intresting_whatever_td_xpath)
+    click_radio_button(driver, contact_availability_whatever_td_xpath)
+    click_radio_button(driver, social_behavior_whatever_td_xpath)
+    click_submit(driver, submit_form_xpath, submit_input_class_name, submit_value)
 
-    attendance_major = driver.find_element(By.XPATH, attendance_major_td_xpath).find_element(By.TAG_NAME, 'img')
-    attendance_major.click();
-    time.sleep(wait_to_fill_time)
 
-    location_at_university = driver.find_element(By.XPATH, location_at_university_td_xpath).find_element(By.TAG_NAME, 'img')
-    location_at_university.click();
-    time.sleep(wait_to_fill_time)
+def click_radio_button(driver, xpath):
+    WebDriverWait(driver, wait_timeout).until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    # without this line radio_button element is stale
+    time.sleep(0.1)
+    radio_button = driver.find_element(By.XPATH, xpath).find_element(By.TAG_NAME, 'img')
+    radio_button.click();
 
-    according_to_rules_whatever = driver.find_element(By.XPATH, according_to_rules_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    according_to_rules_whatever.click() 
-    time.sleep(wait_to_fill_time)
+def click_submit(driver, parent_xpath, input_class_name, value):
+    WebDriverWait(driver, wait_timeout).until(EC.visibility_of_element_located((By.XPATH, parent_xpath)))
+    # without this line element is stale
+    time.sleep(0.1)
+    parent = driver.find_element(By.XPATH, parent_xpath)
+    classes = parent.find_elements(By.CLASS_NAME, input_class_name)
 
-    according_to_course_card_whatever = driver.find_element(By.XPATH, according_to_course_card_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    according_to_course_card_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    results_on_time_whatever = driver.find_element(By.XPATH, results_on_time_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    results_on_time_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    classes_understandable_whatever = driver.find_element(By.XPATH, classes_understandable_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    classes_understandable_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    classes_intresting_whatever = driver.find_element(By.XPATH, classes_intresting_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    classes_intresting_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    contact_availability_whatever = driver.find_element(By.XPATH, contact_availability_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    contact_availability_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    social_behavior_whatever = driver.find_element(By.XPATH, social_behavior_whatever_td_xpath).find_element(By.TAG_NAME, 'img')
-    social_behavior_whatever.click() 
-    time.sleep(wait_to_fill_time)
-
-    # submit = driver.find_element(By.XPATH,'')
-
-    # submit.click()
-    print("waiting")
-    input()
-    print("not")
-    time.sleep(2)
+    for _class in classes:
+        if(_class.get_attribute("value") == value):
+            WebDriverWait(driver, wait_timeout).until(EC.visibility_of(_class))
+            _class.click()
+            return
+        
+##############################
 
 driver = webdriver.Chrome()
 url = "https://moja.pg.edu.pl/auth/app/student/"
 driver.get(url)
 
 try:
-    WebDriverWait(driver, 3600).until(EC.url_to_be(url))
-    time.sleep(wait_to_load_time)
-    enter_forms(driver)
-    time.sleep(wait_to_load_time)
+    WebDriverWait(driver, wait_timeout).until(EC.url_to_be(url))
+
+    click(driver, fill_forms_link_xpath)
+
     fill_forms(driver)
 except TimeoutError:
     print("Desired url was not rendered with in allocated time")
-
-input()
